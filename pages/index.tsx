0be74d5e1fa10990642384cd1122fb5b5d46f2e3
David Taing <adavidtaing@gmail.com>
2021-11-12 18:35:53 +1100

    feat: init header content

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,16 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <div>
-      <header></header>
+      <header>
+        <ul>
+          <li>
+            <h1 className="logo">TEEANG</h1>
+          </li>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Let{"'"}s Talk</li>
+        </ul>
+      </header>
       <main></main>
     </div>
   );
