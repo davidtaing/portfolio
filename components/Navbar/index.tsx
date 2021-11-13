aba9a21f347d5dd9b1d71beda618410405125dee
David Taing <adavidtaing@gmail.com>
2021-11-13 21:08:44 +1100

    refactor: Navbar into separate component

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <h1 className="logo">TEEANG</h1>
+        </li>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Let{"'"}s Talk</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
     <div>
       <header>
-        <ul>
-          <li>
-            <h1 className="logo">TEEANG</h1>
-          </li>
-          <li>Home</li>
-          <li>Projects</li>
-          <li>Let{"'"}s Talk</li>
-        </ul>
+        <Navbar />
       </header>
       <main></main>
     </div>
