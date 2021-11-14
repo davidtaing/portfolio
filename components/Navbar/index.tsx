5cb7a3d4a3b3742ee10b3446126aaa638827a002
David Taing <adavidtaing@gmail.com>
2021-11-14 21:48:26 +1100

    styles: adding styling to Navbar component

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,14 +1,47 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  max-width: 1440px;
+  max-height: 60px;
+  margin: 0 auto;
+  display: flex;
+  list-style: none;
+  align-items: center;
+  justify-content: flex-end;
+
+  li {
+    padding: 8px 16px;
+  }
+
+  h1 {
+    display: inline;
+  }
+
+  .nav-left {
+    margin-right: auto;
+  }
+`;
+
 const Navbar = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <h1 className="logo">TEEANG</h1>
+      <StyledUl>
+        <li className="nav-logo nav-left">
+          <h1 className="logo">
+            <Link href="/">TEEANG</Link>
+          </h1>
         </li>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>Let{"'"}s Talk</li>
-      </ul>
+        <li className="nav-right">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="nav-right">
+          <Link href="/projects">Projects</Link>
+        </li>
+        <li className="nav-right">
+          <Link href="/contact">{"Let's Talk"}</Link>
+        </li>
+      </StyledUl>
     </div>
   );
 };
