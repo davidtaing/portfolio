9a41c720b318ac39c8ded1963e0fad4f9f753862
David Taing <adavidtaing@gmail.com>
2021-11-16 11:18:40 +1100

    styles: added structure and styling to svg element

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,6 +71,29 @@ const StyledDiv = styled.div`
   }
 `;
 
+const StyledSvg = styled.svg`
+  display: inline;
+  position: relative;
+`;
+
+const SvgElement = () => (
+  <StyledSvg
+    xmlns="http://www.w3.org/2000/svg"
+    width="92.853"
+    height="48.39"
+    viewBox="0 0 92.853 48.39"
+  >
+    <path
+      id="Path"
+      d="M3186.127,1855.291l49.467-38.495-8.692,38.495L3258.7,1831.3l-3.414,23.994,19.352-14.108-3.875,17.236"
+      transform="translate(-3184.899 -1811.786)"
+      fill="none"
+      stroke="rgba(33,33,33,0.85)"
+      strokeWidth="4"
+    />
+  </StyledSvg>
+);
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -83,21 +106,8 @@ const Home: NextPage = () => {
             <h1 className="heading">
               I design and build great fullstack applications
             </h1>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="92.853"
-              height="48.39"
-              viewBox="0 0 92.853 48.39"
-            >
-              <path
-                id="Path"
-                d="M3186.127,1855.291l49.467-38.495-8.692,38.495L3258.7,1831.3l-3.414,23.994,19.352-14.108-3.875,17.236"
-                transform="translate(-3184.899 -1811.786)"
-                fill="none"
-                stroke="rgba(33,33,33,0.85)"
-                strokeWidth="4"
-              />
-            </svg>
+
+            <SvgElement />
             <h2 className="subheading">
               A Sydney Based Developer focusing on TypeScript, React, Next.js &
               Express.
