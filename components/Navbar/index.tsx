7d12119284e3528a96280a06d9ae2ec80d20fad0
David Taing <adavidtaing@gmail.com>
2021-11-20 19:41:53 +1100

    feat: navbar
    Markup basic structure.

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,8 +1,35 @@
 import Link from "next/link";
 import styled from "styled-components";
 
+const StyledNav = styled.nav`
+  background-color: #212121;
+`;
+
 const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <StyledNav>
+      <Link href="/">
+        <a className="nav-logo">TEEANG</a>
+      </Link>
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <Link href="/">
+            <a className="nav-link">Home</a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/projects">
+            <a className="nav-link">My Projects</a>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link href="/contact">
+            <a className="nav-link">Contact Me</a>
+          </Link>
+        </li>
+      </ul>
+    </StyledNav>
+  );
 };
 
 export default Navbar;
