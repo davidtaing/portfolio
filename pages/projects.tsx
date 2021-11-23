7c4a3b9d774a73ce0b2486ecd9afd370b05b0f37
David Taing <adavidtaing@gmail.com>
2021-11-23 11:45:59 +1100

    feat: init stub Contact and Projects pages

---
+++ pages/projects.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+import TabBar from "../components/TabBar";
+
+const Projects: NextPage = () => {
+  return <div></div>;
+};
+
+export default Projects;
