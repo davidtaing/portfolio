a1700ea6458661b6c8ddd61d92cde48167962e10
David Taing <adavidtaing@gmail.com>
2021-11-26 21:46:37 +1100

    feat: homepage/hero
    - style Hero container elements using styled component
    - added line breaks to headline and subheadline
    - added display: grid to global .wrapper class

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,14 +1,75 @@
 import Image from "next/image";
+import styled from "styled-components";
 import profilePic from "../../../public/portrait-1024.png";
+
+const StyledDiv = styled.div`
+  display: grid;
+  grid-template-areas:
+    "image"
+    "text";
+
+  .hero-container--image,
+  .hero-container--text {
+    margin: 1rem 1.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .hero-container--text {
+    grid-area: text;
+  }
+
+  .hero-container--image {
+    grid-area: image;
+  }
+
+  @media (min-width: 768px) {
+    grid-template-columns: 3fr 1fr 2fr;
+    grid-template-areas: "text . image";
+    height: calc(100vh - 60px);
+
+    .hero-container {
+      display: flex;
+      align-items: center;
+    }
+
+    .hero-container--text {
+      .headline {
+        margin: 2rem 0;
+        font-weight: 500;
+        font-size: 5rem;
+        line-height: 5.25rem;
+      }
+
+      .subheadline {
+        margin: 2rem 0;
+        font-weight: 300;
+        font-size: 2rem;
+        line-height: 3rem;
+      }
+    }
+  }
+`;
 
 const HeroSection = () => {
   return (
-    <div id="home" className="hero-section">
-      <div id="hero-section--text">
-        <h1>Creating Web Experiences That Move You.</h1>
-        <h2>
-          Sydney Based Fullstack Developer with a focus on TypeScript & React.
+    <StyledDiv id="home" className="hero-section">
+      <div className="hero-container--text">
+        <h1 className="headline text-primary">
+          Creating Web
+          <br />
+          Experiences
+          <br />
+          That Move You.
+        </h1>
+        <h2 className="subheadline text-secondary">
+          Sydney Based Fullstack Developer
+          <br />
+          with a focus on TypeScript & React.
         </h2>
+      </div>
+      <div className="hero-container--image">
         <Image
           src={profilePic}
           alt="A picture of me! Hello."
@@ -17,8 +78,7 @@ const HeroSection = () => {
           layout="responsive"
         />
       </div>
-      <div id="hero-section--image"></div>
-    </div>
+    </StyledDiv>
   );
 };
 
