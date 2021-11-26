2965f788c0e56782bda54db7acb9224aac55c2f4
David Taing <adavidtaing@gmail.com>
2021-11-26 20:01:37 +1100

    feat: reimplement homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,61 +3,13 @@ import Image from "next/image";
 import styled from "styled-components";
 import HeroSection from "../components/HeroSection";
 
-const StyledDiv = styled.div`
-  .homepage-content {
-    height: calc(100vh - 120px);
-    margin: 0;
-
-    .homepage-container {
-      margin: 1rem 1.5rem;
-      width: auto;
-
-      .text-container {
-        display: flex;
-        flex-direction: column;
-
-        .headline {
-          padding: 0;
-          margin: 0;
-          font-weight: 500;
-          font-size: 2.5rem;
-          line-height: 2.75rem;
-          text-align: left;
-        }
-
-        .sub-headline {
-          font-weight: normal;
-          font-size: 1.375rem;
-          line-height: 1.75rem;
-        }
-      }
-    }
-  }
-
-  @media (min-width: 768px) {
-    .homepage-content {
-      max-height: 1000px;
-      height: calc(100vh - 60px);
-    }
-  }
-`;
+const StyledDiv = styled.div``;
 
 const Home: NextPage = () => {
   return (
     <StyledDiv className="homepage">
       <div className="wrapper">
-        <div className="homepage-content">
-          <div className="homepage-container">
-            <div className="text-container">
-              <h1 className="headline text-primary">
-                I design and build great fullstack applications.
-              </h1>
-              <h2 className="sub-headline text-secondary">
-                Sydney based developer with a focus on TypeScript, React & Next.
-              </h2>
-            </div>
-          </div>
-        </div>
+        <HeroSection />
       </div>
     </StyledDiv>
   );
