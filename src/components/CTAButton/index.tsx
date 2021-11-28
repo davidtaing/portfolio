74554abc05ff1978aee6d5ede5b4a9b9dfa40c7f
David Taing <adavidtaing@gmail.com>
2021-11-28 17:44:12 +1100

    refactor: add Props interface to CTAButton

--- src/components/CTAButton/index.tsx
+++ src/components/CTAButton/index.tsx
@@ -1,7 +1,13 @@
+import { FC } from "react";
 import Link from "next/link";
 import Button from "../Button";
 
-const CTAButton = ({ href, text }) => (
+interface Props {
+  href: string;
+  text: string;
+}
+
+const CTAButton: FC<Props> = ({ href, text }) => (
   <Link href={href}>
     <a>
       <Button className="cta-button" text={text} />
