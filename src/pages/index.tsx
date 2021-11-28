80d396243a43e4382aca78c2249516d7565bf57e
David Taing <adavidtaing@gmail.com>
2021-11-28 18:52:31 +1100

    feat: added stub projects section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,61 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 import styled from "styled-components";
 import HeroSection from "../components/HeroSection";
 
-const StyledDiv = styled.div``;
+const StyledDiv = styled.div`
+  .projects-section {
+    background-color: white;
+    width: 100%;
+    min-height: calc(100vh - 60px);
+
+    @media (min-width: 768px) {
+      min-height: 100vh;
+    }
+  }
+`;
 
 const Home: NextPage = () => {
   return (
     <StyledDiv className="homepage">
       <HeroSection />
+      <section id="projects" className="projects-section">
+        <div className="wrapper">
+          <h1>Projects</h1>
+          <ul>
+            <li>
+              <h2>portfolio</h2>
+              <span>TypeScript</span>
+              <span>React</span>
+              <span>Next.js</span>
+              <span>CSS</span>
+              <p>
+                My portfolio website. Built with Next.js and TypeScript. Styled
+                with CSS.
+              </p>
+            </li>
+            <li>
+              <h2>photo-sorter</h2>
+              <span>JavaScript</span>
+              <span>Node</span>
+              <p>
+                Loads exif metadata from image files and sorts them into folders
+                by date. Used to sort over 17,000 photos accumulated from my
+                real estate work.
+              </p>
+            </li>
+            <li>
+              <h2>email-sender</h2>
+              <span>TypeScript</span>
+              <span>Node</span>
+              <span>SendGrid API</span>
+              <p>
+                Loads data from a spreadsheet and batch sends emails with
+                individual attachments using the SendGrid API.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
     </StyledDiv>
   );
 };
