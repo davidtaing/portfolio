b3782e45e258c46a58d05c0c610edc848e6d11be
David Taing <adavidtaing@gmail.com>
2021-11-30 13:42:05 +1100

    style: ProjectCard and ProjectsSection
    Project Card:
    - Added category property to ProjectCard.
    - Rearranged order of JSX elements.
         - Skills are now at the bottom as per the design spec.
    - Wrapped "View {previewType}" in strong element.
    
    ProjectsSection
    - convert ProjectsSection ul selector to class selector.
    - fixed preview-type typos
    - changed .projects-list to display flex.

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styled from "styled-components";
 
 interface Props {
+  category: string;
   title: string;
   skills: Array<string>;
   imageUrl: string;
@@ -13,30 +14,61 @@ interface Props {
 
 const StyledLi = styled.li`
   display: grid;
-  border: solid;
   margin: 2rem 0;
+
+  .project-card--text {
+    .project-card--category {
+      margin-top: 0;
+      margin-bottom: 0.5rem;
+      font-size: 24px;
+      color: #474747;
+    }
+
+    .project-card--title {
+      font-size: 40px;
+      color: #000000;
+    }
+
+    .project-card--body {
+      font-size: 24px;
+      color: #272727;
+    }
+
+    .project-card--skill-list {
+      padding: 0;
+      flex-direction: row;
+      font-size: 18px;
+      color: #474747;
+    }
+
+    .project-card--skill-list > * {
+      margin: 1rem 0.75rem 1rem 0;
+      display: inline;
+    }
+  }
 
   @media (min-width: 768px) {
     grid-template-columns: 1fr 1fr;
     grid-template-areas: "text image";
 
-    .project-card-text,
-    .project-card-image {
+    .project-card--text,
+    .project-card--image {
       margin: 2rem 0;
     }
 
-    .project-card-text {
+    .project-card--text {
       grid-area: text;
       max-width: 80%;
     }
 
-    .project-card-image {
+    .project-card--image {
       grid-area: image;
     }
   }
 `;
 
 const ProjectCard: FC<Props> = ({
+  category,
   title,
   skills,
   imageUrl,
@@ -45,7 +77,7 @@ const ProjectCard: FC<Props> = ({
   url,
 }) => (
   <StyledLi className="project-card">
-    <div className="project-card-image">
+    <div className="project-card--image">
       <Image
         src={imageUrl}
         alt={imageUrl}
@@ -54,22 +86,30 @@ const ProjectCard: FC<Props> = ({
         layout="responsive"
       />
     </div>
-    <div className="project-card-text">
-      <h2>{title}</h2>
-      <ul className="skill-list">
+    <div className="project-card--text">
+      <p className="project-card--category">{category}</p>
+      <h2 className="project-card--title">{title}</h2>
+      <p className="project-card--body">
+        {bodyText}
+        {previewType ? (
+          <p>
+            <strong>View {previewType}:</strong>{" "}
+            <a
+              className="project-card--preview-url"
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {url}
+            </a>
+          </p>
+        ) : null}
+      </p>
+      <ul className="project-card--skill-list">
         {skills.map((item) => (
           <li key={`${title}-${item}`}>{item}</li>
         ))}
       </ul>
-      <p>{bodyText}</p>
-      {previewType ? (
-        <p>
-          {previewType}:{" "}
-          <a href={url} target="_blank" rel="noreferrer">
-            {url}
-          </a>
-        </p>
-      ) : null}
     </div>
   </StyledLi>
 );
--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -4,24 +4,22 @@ import ProjectCard from "../ProjectCard";
 const StyledSection = styled.section`
   .wrapper {
     .content {
-      margin: 2rem 1.5rem;
+      margin: 3rem 1.5rem;
 
       h1 {
         font-size: var(--fs-xl);
         line-height: 1.2;
       }
 
-      ul {
-        display: grid;
+      .projects-list {
+        display: flex;
+        flex-direction: column;
         list-style: none;
         margin: 0;
         padding: 0;
 
         h2 {
           font-size: var(--fs-600);
-        }
-
-        .skill-list {
         }
       }
     }
@@ -35,28 +33,31 @@ const ProjectsSection = () => (
         <h1>Projects</h1>
         <ul className="projects-list">
           <ProjectCard
+            category="Website"
             title="portfolio"
             skills={["TypeScript", "React", "Next.js", "CSS"]}
             imageUrl="/project-mockups/portfolio.png"
             bodyText="You are here. My portfolio website to showcase my projects."
           />
           <ProjectCard
+            category="Node App"
             title="photo-sorter"
             skills={["JavaScript", "Node"]}
             imageUrl="/project-mockups/photo-sorter.png"
             bodyText="Loads exif metadata from image files and sorts them into folders
             by date. Used to sort over 17,000 photos accumulated from my real
             estate work."
-            previewType="Demostration"
+            previewType="Demonstration"
             url="https://youtu.be/cL8a5ofU_W4"
           />
           <ProjectCard
+            category="Node App"
             title="email-sender"
             skills={["TypeScript", "Node", "SendGrid API", "CSS"]}
             imageUrl="/project-mockups/email-sender.png"
             bodyText="Loads data from a spreadsheet and batch sends emails with
             individual attachments using the SendGrid API."
-            previewType="Demostration"
+            previewType="Demonstration"
             url="https://youtu.be/G0a3LBJhtfY"
           />
         </ul>
