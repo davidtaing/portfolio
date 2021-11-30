f2be82471ddefa9ec5dfd01ed74c53e628253df5
David Taing <adavidtaing@gmail.com>
2021-11-30 22:43:24 +1100

    test: api/contact
    - Added test for DELETE requests

--- __tests__/api/contact.test.ts
+++ __tests__/api/contact.test.ts
@@ -1,6 +1,24 @@
+import handler from "../../src/pages/api/contact";
+import { createMocks } from "node-mocks-http";
+
+jest.mock("@sendgrid/mail");
+
 describe("/api/contact", () => {
-  it("fails test", () => {
-    expect(true).toBe(false);
+  describe("Rejects DELETE Request", () => {
+    const { req, res } = createMocks({
+      method: "DELETE",
+    });
+
+    beforeAll(() => handler(req, res));
+
+    test("Responds with 405 Status Code", () => {
+      expect(res._getStatusCode()).toBe(405);
+    });
+
+    test("Responds with JSON Message: 'Method Not Allowed'", () => {
+      const { message: actualMessage } = res._getJSONData();
+      expect(actualMessage).toBe("Method Not Allowed");
+    });
   });
 });
 
