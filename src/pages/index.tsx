d2b5fce741a30c20a6e1535dee061c170e3410e5
David Taing <adavidtaing@gmail.com>
2021-11-30 23:30:36 +1100

    wrapped homepage sections in ActiveSectionProvider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,16 @@ import type { NextPage } from "next";
 import ContactSection from "../components/contact/ContactSection";
 import HeroSection from "../components/hero/HeroSection";
 import ProjectsSection from "../components/projects/ProjectsSection";
+import ActiveSectionProvider from "../contexts/ActiveSectionContext";
 
 const Home: NextPage = () => {
   return (
     <div className="homepage">
-      <HeroSection />
-      <ProjectsSection />
-      <ContactSection />
+      <ActiveSectionProvider>
+        <HeroSection />
+        <ProjectsSection />
+        <ContactSection />
+      </ActiveSectionProvider>
     </div>
   );
 };
