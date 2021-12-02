ffb2e30faa4bd61777e13bb9364439684401a71a
David Taing <adavidtaing@gmail.com>
2021-12-02 23:40:01 +1100

    feat: moved ActiveSectionProvider up to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/navigation/Header";
 import TabBar from "../components/navigation/TabBar";
+import ActiveSectionProvider from "../contexts/ActiveSectionContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="app">
-      <Header />
-      <Component {...pageProps} />
-      <TabBar />
+      <ActiveSectionProvider>
+        <Header />
+        <Component {...pageProps} />
+        <TabBar />
+      </ActiveSectionProvider>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,13 @@ import type { NextPage } from "next";
 import ContactSection from "../components/contact/ContactSection";
 import HeroSection from "../components/hero/HeroSection";
 import ProjectsSection from "../components/projects/ProjectsSection";
-import ActiveSectionProvider from "../contexts/ActiveSectionContext";
 
 const Home: NextPage = () => {
   return (
     <div className="homepage">
-      <ActiveSectionProvider>
-        <HeroSection />
-        <ProjectsSection />
-        <ContactSection />
-      </ActiveSectionProvider>
+      <HeroSection />
+      <ProjectsSection />
+      <ContactSection />
     </div>
   );
 };
