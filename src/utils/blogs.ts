f147f974786961e7b3212c0f350ffae5a91649c6
David Taing <adavidtaing@gmail.com>
2021-12-06 11:29:13 +1100

    refactor: moved query into const

--- src/utils/blogs.ts
+++ src/utils/blogs.ts
@@ -1,31 +1,33 @@
 import "isomorphic-fetch";
 
+const query = `query GetPosts{
+  posts{
+      edges{
+      node{
+        title
+        categories{
+        edges{
+            node{
+              name
+            }
+          }
+        }
+        excerpt
+        slug
+        content
+      }
+    }
+  }
+ }`;
+
 export const getBlogs = async () => {
-  const response = await fetch(process.env.WORDPRESS_API_URL as string, {
+  const response = await fetch("https://blog.teeang.net/graphql", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      query: `
-    query postsQuery{
-      posts{
-          edges{
-          node{
-              title
-              categories{
-              edges{
-                  node{
-                  name
-                  }
-              }
-              }
-              excerpt
-              slug
-              content
-          }
-          }
-      }
-     }`,
+      query: query,
     }),
   });
-  return (await response.json()).data();
+
+  return await response.json();
 };
