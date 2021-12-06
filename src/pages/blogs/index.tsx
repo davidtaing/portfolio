4669c1e3b030ae76a76aab69259c32e6a43d80b1
David Taing <adavidtaing@gmail.com>
2021-12-06 23:09:31 +1100

    fix: blogs not loading
    when client visits non-home page as their first page.

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from "react";
 import type { NextPage } from "next";
+import { GetStaticProps } from "next";
+
+// Contexts
 import { useBlogsContext } from "../../common/contexts/BlogsContext";
 
-const Blogs: NextPage = () => {
-  const { blogs } = useBlogsContext();
+// Components
+import { getBlogs } from "../../utils/blogs";
+
+// Types
+import { Post } from "../../utils/blogs";
+
+const Blogs: NextPage<{ data: [Post] }> = ({ data }) => {
+  const { blogs, setBlogs } = useBlogsContext();
+
+  useEffect(() => {
+    setBlogs(data);
+  }, [data, setBlogs]);
+
   return (
     <div className="blogs">
       <div className="wrapper">
@@ -32,6 +47,19 @@ const Blogs: NextPage = () => {
       </div>
     </div>
   );
+};
+
+type Props = {
+  data: [Post];
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await getBlogs();
+  return {
+    props: {
+      data: data,
+    },
+  };
 };
 
 export default Blogs;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,16 @@
-import { useEffect } from "react";
 import type { NextPage } from "next";
-import { GetStaticProps } from "next";
-
-// Utils
-import { getBlogs } from "../utils/blogs";
-
-// Contexts
-import { useBlogsContext } from "../common/contexts/BlogsContext";
 
 // Components
 import HeroSection from "../common/components/hero/HeroSection";
 import ProjectsSection from "../common/components/projects/ProjectsSection";
 
-// Types
-import { Post } from "../utils/blogs";
-
-const Home: NextPage<{ data: [Post] }> = ({ data }) => {
-  const { setBlogs } = useBlogsContext();
-
-  useEffect(() => {
-    setBlogs(data);
-  }, [data, setBlogs]);
-
+const Home: NextPage = () => {
   return (
     <div className="homepage">
       <HeroSection />
       <ProjectsSection />
     </div>
   );
-};
-
-type Props = {
-  data: [Post];
-};
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const data = await getBlogs();
-  return {
-    props: {
-      data: data,
-    },
-  };
 };
 
 export default Home;
