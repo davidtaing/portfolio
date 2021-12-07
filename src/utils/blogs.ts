1ff0b373b73bd48d4433b6b49bdd43806b2c0280
David Taing <adavidtaing@gmail.com>
2021-12-07 22:35:59 +1100

    refactor: replace hardcorded url with env variable

--- src/utils/blogs.ts
+++ src/utils/blogs.ts
@@ -19,7 +19,7 @@ export type Post = {
 
 const executeQuery = async (query: string, variables?: any) => {
   try {
-    const response = await fetch("https://blog.teeang.net/graphql", {
+    const response = await fetch(process.env.WORDPRESS_API_URL as string, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
