e9cd447c474a1d42f3dc8927de44af3489d237eb
David Taing <adavidtaing@gmail.com>
2021-12-12 22:27:16 +1100

    style: navbar
    font is now white for logo and light-grey for menu text

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,50 @@
 import { FC } from "react";
 import Link from "next/link";
+import styled from "styled-components";
 
 interface Props {}
 
+const StyledHeader = styled.header`
+  height: 60px;
+  border: solid;
+  border-color: white;
+
+  .wrapper {
+    align-items: center;
+
+    .logo {
+      display: inline;
+      margin-right: auto;
+      color: var(--color-light-primary);
+      font-style: italic;
+    }
+
+    ul {
+      display: inline;
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+
+      li {
+        display: inline;
+        margin-left: 2.5rem;
+        a {
+          color: var(--color-light-secondary);
+        }
+      }
+    }
+  }
+`;
+
 const Navbar: FC<Props> = () => {
   return (
-    <header>
-      <div className="wrapper">
-        <Link href="/">
-          <a>TEEANG</a>
-        </Link>
+    <StyledHeader>
+      <div className="wrapper flex">
+        <div className="logo">
+          <Link href="/">
+            <a>TEEANG</a>
+          </Link>
+        </div>
         <ul>
           <li>
             <Link href="/">
@@ -33,7 +68,7 @@ const Navbar: FC<Props> = () => {
           </li>
         </ul>
       </div>
-    </header>
+    </StyledHeader>
   );
 };
 
