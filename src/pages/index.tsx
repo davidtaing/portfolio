11418ca6786d4710c15698edbf25abbe274bdb77
David Taing <adavidtaing@gmail.com>
2021-12-13 10:19:27 +1100

    refactor: HeroSection to separate container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,10 @@
 import type { NextPage } from "next";
-import styled from "styled-components";
-
-const StyledSection = styled.section`
-  height: calc(100vh - 60px);
-
-  .content {
-    margin: auto 0;
-    justify-content: center;
-
-    .cta-button-container {
-      > * {
-        margin-top: 3rem;
-      }
-
-      .contact-btn {
-        display: none;
-        margin-left: 2rem;
-
-        @media (min-width: 50em) {
-          display: inline;
-        }
-      }
-    }
-  }
-`;
+import HeroSection from "../containers/HeroSection/HeroSection";
 
 const Home: NextPage = () => {
   return (
     <div className="homepage">
-      <StyledSection className="hero-section">
-        <div className="wrapper grid-col-12">
-          <div className="content">
-            <h1 className="fs-xl clr-light-primary">
-              Turn your ideas into websites & apps.
-            </h1>
-            <h2 className="fs-l clr-light-secondary">
-              Sydney Based Fullstack Developer with a focus on TypeScript, React
-              & Express.
-            </h2>
-            <div className="cta-button-container">
-              <button className="select-work-btn btn-primary fs-h600">
-                View Select Work
-              </button>
-              <button className="contact-btn btn-secondary fs-h600">
-                Or Contact Me
-              </button>
-            </div>
-          </div>
-        </div>
-      </StyledSection>
+      <HeroSection />
     </div>
   );
 };
