a2f2ba49161a605d40238e98af3d67ec47f8c328
David Taing <adavidtaing@gmail.com>
2021-12-12 21:10:00 +1100

    feat: add html elements to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className="homepage"></div>;
+  return (
+    <div className="homepage">
+      <section className="hero-section">
+        <h1>
+          Turn your ideas
+          <br />
+          into websites & apps.
+        </h1>
+        <h2>
+          Sydney Based Fullstack Developer
+          <br />
+          with a focus on TypeScript, React & Express.
+        </h2>
+        <button className="button-primary">View Select Work</button>
+        <button className="button-secondary">Or Contact Me</button>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
