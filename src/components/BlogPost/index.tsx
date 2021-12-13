43c5909032380fbe77c2ea361557c8fd9014ae2f
David Taing <adavidtaing@gmail.com>
2021-12-13 23:00:18 +1100

    style: fix wordpress images breaking alignment

--- src/components/BlogPost/index.tsx
+++ src/components/BlogPost/index.tsx
@@ -1,15 +1,29 @@
 import { FC } from "react";
+import styled from "styled-components";
 
 interface Props {
   title: string;
   content: string;
 }
 
+const StyledDiv = styled.div`
+  div {
+    figure {
+      margin: 0;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+`;
+
 const BlogPost: FC<Props> = ({ title, content }) => (
-  <div className="blogpost">
+  <StyledDiv className="blogpost">
     <h1 className="title">{title}</h1>
     <div dangerouslySetInnerHTML={{ __html: content }} />
-  </div>
+  </StyledDiv>
 );
 
 export default BlogPost;
