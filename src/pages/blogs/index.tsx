0750abbc878c5e19a15acbc566f2df3d730efe79
David Taing <adavidtaing@gmail.com>
2021-12-13 17:37:55 +1100

    feat: blog
    - added links to individual blog pages

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 import { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
 
 // Contexts
 import { useBlogsContext } from "../../contexts/BlogsContext";
 
 // Utils
 import { getBlogs } from "../../utils/blogs";
-
-// Components
 
 // Types
 import { Post } from "../../utils/blogs";
@@ -23,19 +22,32 @@ const Blogs: NextPage<{ data: Post[] }> = ({ data }) => {
   return (
     <section className="blogs">
       <div className="wrapper content-wrapper">
-        {blogs.map(({ node }) => (
-          <article key={node.slug} className="blogpost-card">
-            <h1 className="title">{node.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-          </article>
-        ))}
+        {blogs.map(({ node }) => {
+          const blogUrl = `/blogs/${node.slug}`;
+
+          return (
+            <article key={node.slug} className="blogpost-card">
+              <Link href={blogUrl}>
+                <a>
+                  <h1 className="title">{node.title}</h1>
+                </a>
+              </Link>
+              <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+              <Link href={blogUrl}>
+                <a>
+                  <p className="read-more-linl">Read More.</p>
+                </a>
+              </Link>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
 };
 
 type Props = {
-  data: [Post];
+  data: Post[];
 };
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
