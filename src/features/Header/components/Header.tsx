8faf932e7d071c7676bfbac96a7834694b5f967c
David Taing <adavidtaing@gmail.com>
2022-04-28 22:35:14 +1000

    feat: Header component
    Summary:
    - Add Logo.svg
    - Implement header logo as a Next Image
    - Add wrapper, flex & grid utility classes to globals.css

--- src/features/Header/components/Header.tsx
+++ src/features/Header/components/Header.tsx
@@ -1,11 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  border-bottom: 2px solid #4f4f4f;
+  height: 60px;
+
+  ul {
+    display: inline-flex;
+    justify-content: end;
+    gap: 1.5rem;
+    padding: 0;
+    margin-left: auto;
+
+    li {
+      list-style: none;
+      display: inline;
+    }
+  }
+`;
+
 export function Header() {
   return (
-    <header>
-      <nav className="nav-menu">
-        <div className="wrapper">
-          <a className="nav-logo" href="/">
-            Teeang
-          </a>
+    <StyledHeader>
+      <div className="wrapper">
+        <nav className="nav-menu flex">
+          <Link href="/" passHref={true}>
+            <Image
+              alt="Logo"
+              src="/Logo.svg"
+              layout="intrinsic"
+              width="90px"
+              height="27px"
+            />
+          </Link>
           <ul className="nav-list">
             <li className="nav-item">
               <a href="/projects">Projects</a>
@@ -20,8 +48,8 @@ export function Header() {
               <a href="contact-me">Contact Me</a>
             </li>
           </ul>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </div>
+    </StyledHeader>
   );
 }
