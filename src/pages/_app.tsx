4286c55ac62fa75d3dc40ec7ac18d2868744ddea
David Taing <adavidtaing@gmail.com>
2022-04-28 21:47:26 +1000

    Delete Files in Preparation for Rewrite

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-// Components
-import Navbar from "../components/Navbar";
-import Tabbar from "../components/Tabbar";
-import BlogsContextProvider from "../contexts/BlogsContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div className="app">
-      <Navbar />
-      <BlogsContextProvider>
-        <Component {...pageProps} />
-      </BlogsContextProvider>
-      <Tabbar />
-    </div>
-  );
+  return (<Component {...pageProps} />);
 }
 
 export default MyApp;
