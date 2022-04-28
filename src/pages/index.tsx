4564acd28827bdfe41258aba4e5401b1be18d1ff
David Taing <adavidtaing@gmail.com>
2022-04-28 21:47:26 +1000

    Delete Files in Preparation for Rewrite

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
 import type { NextPage } from "next";
-import HeroSection from "../containers/HeroSection";
-import SelectWorkSection from "../containers/SelectWorkSection";
 
 const Home: NextPage = () => {
   return (
     <div className="homepage">
-      <HeroSection />
-      <SelectWorkSection />
     </div>
   );
 };
