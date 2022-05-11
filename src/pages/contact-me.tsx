706b720d302bac0f5c872af4f67a5fd8f4fabae8
David Taing <adavidtaing@gmail.com>
2022-05-11 21:11:14 +1000

    feat: Add Stub Pages for Other Pages

---
+++ src/pages/contact-me.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+import { Header } from "../features/Header";
+
+const Contact: NextPage = () => {
+  return (
+    <div className="contact">
+      <Header />
+      <div className="wrapper">
+        <div className="text-container">
+          <h1 className="fluid-heading-05">Page Under Construction.</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/projects.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import { Header } from "../features/Header";
+
+const Projects: NextPage = () => {
+  return (
+    <div className="projects">
+      <Header />
+      <div className="wrapper">
+        <div className="text-container">
+          <h1 className="fluid-heading-05">Page Under Construction.</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
