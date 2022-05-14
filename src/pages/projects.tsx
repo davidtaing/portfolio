72b2ea559608be9b61c19d9e39d481b2de5bee75
David Taing <adavidtaing@gmail.com>
2022-05-14 22:41:59 +1000

    Feat/projects list page (#5)
    * style: Add .grid-layout Util Class
    
    Summary:
    - Defines the grid column layouts for:
        - Mobile - 4 columns
        - Tablet - 8 columns
        - Desktop - 12 columns
    
    * feat: Projects List Page
    
    Summary:
    - Replaced header with Projects
    
    * feat: Projects List Page
    
    Summary:
    - Add test data array with five empty objects
    - Add ProjectsCard component
    
    * feat: Projects List Page
    
    Summary:
    - Add .grid-layout to Projects List Page
    
    * feat: Projects List Page
    
    Summary:
    - Refactor ProjectsCard into separate file.
    - Style ProjectsCard as squares.
    
    * feat: Projects List Page
    
    Summary:
    - Adjust ProjectsCard aspect-ratio to 3/2
    - Rename ProjectsCard to ProjectCard

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from "next";
 import { Header } from "../features/Header";
+import { ProjectCard } from "../features/Projects";
+
+const data = new Array(5).fill({});
 
 const Projects: NextPage = () => {
   return (
     <div className="projects">
       <Header />
       <div className="wrapper">
-        <div className="text-container">
-          <h1 className="fluid-heading-05">Page Under Construction.</h1>
+        <div className="text-container grid-layout">
+          <h1 className="fluid-heading-05">Projects</h1>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
         </div>
       </div>
     </div>
