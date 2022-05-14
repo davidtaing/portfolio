fcce70b3ec2338636fed6da516a5ce6347b1349b
David Taing <adavidtaing@gmail.com>
2022-05-14 22:58:37 +1000

    style: ProjectsList

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,10 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
 import { ProjectCard } from "../features/Projects";
+
+const StyledDiv = styled.div`
+  margin-top: 2.5rem;
+`;
 
 const data = new Array(5).fill({});
 
@@ -7,13 +12,15 @@ const Projects: NextPage = () => {
   return (
     <div className="projects">
       <div className="wrapper">
-        <div className="text-container grid-layout">
+        <div className="text-container">
           <h1 className="fluid-heading-05">Projects</h1>
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          <StyledDiv className="project-list__container grid-layout">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </StyledDiv>
         </div>
       </div>
     </div>
