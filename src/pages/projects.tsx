1514d330bf4d9e155fb07c182d0843a557299501
David Taing <adavidtaing@gmail.com>
2022-05-14 20:54:42 +1000

    feat: Projects List Page
    Summary:
    - Add test data array with five empty objects
    - Add ProjectsCard component

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from "next";
 import { Header } from "../features/Header";
 
+const data = new Array(5).fill({});
+
+function ProjectsCard() {
+  return <div className="projects-card">ProjectsCard</div>;
+}
+
 const Projects: NextPage = () => {
   return (
     <div className="projects">
@@ -8,6 +14,9 @@ const Projects: NextPage = () => {
       <div className="wrapper">
         <div className="text-container">
           <h1 className="fluid-heading-05">Projects</h1>
+          {data.map((project, index) => (
+            <ProjectsCard key={index} />
+          ))}
         </div>
       </div>
     </div>
