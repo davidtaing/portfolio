9e0efbd0400d8d630590b49fb31615d0b64667f5
David Taing <adavidtaing@gmail.com>
2022-05-25 15:27:46 +1000

    style(NavLogo): Fix .underline Classname

--- src/features/Header/components/NavLogo.tsx
+++ src/features/Header/components/NavLogo.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import styled from "styled-components";
+
+const StyledA = styled.a`
+  .underline {
+    text-decoration: underline;
+  }
+`;
 
 export function NavLogo() {
   return (
     <Link href="/" passHref={true}>
-      <a className="nav-menu__logo">
-        <span className="text--underline">Tee</span>ang
-      </a>
+      <StyledA className="nav-menu__logo">
+        <span className="underline">Tee</span>ang
+      </StyledA>
     </Link>
   );
 }
