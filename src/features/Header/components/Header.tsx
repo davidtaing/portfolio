da13e2cef03025de49c904c9ad729466395de3cd
David Taing <adavidtaing@gmail.com>
2022-05-25 14:55:37 +1000

    reactor: NavbarMenu to Seperate File

--- src/features/Header/components/Header.tsx
+++ src/features/Header/components/Header.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import Link from "next/link";
+import { NavbarMenu } from "./NavbarMenu";
 
 export function Header() {
   return (
@@ -15,43 +15,5 @@ export function Header() {
         </nav>
       </div>
     </header>
-  );
-}
-
-export function NavbarMenu() {
-  const [visible, setVisible] = useState(false);
-  const visibileClassName = ` ${visible ? "visible" : ""}`;
-
-  return (
-    <div className={`nav-menu ${visible ? "active" : ""}`}>
-      <ul className="nav-menu__list flex">
-        <span
-          className="hamburger-menu material-symbols-outlined"
-          onClick={() => setVisible(!visible)}
-        >
-          menu
-        </span>
-        <li className={"nav-menu__item" + visibileClassName}>
-          <Link href="/">
-            <a className="nav-menu__link">Home</a>
-          </Link>
-        </li>
-        <li className={"nav-menu__item" + visibileClassName}>
-          <Link href="/projects">
-            <a className="nav-menu__link">Projects</a>
-          </Link>
-        </li>
-        <li className={"nav-menu__item" + visibileClassName}>
-          <Link href="/blogs">
-            <a className="nav-menu__link">Blogs</a>
-          </Link>
-        </li>
-        <li className={"nav-menu__item" + visibileClassName}>
-          <Link href="/contact-me">
-            <a className="nav-menu__link">Contact Me</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
   );
 }
---
+++ src/features/Header/components/NavbarMenu.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export function NavbarMenu() {
+  const [visible, setVisible] = useState(false);
+  const visibileClassName = ` ${visible ? "visible" : ""}`;
+
+  return (
+    <div className={`nav-menu ${visible ? "active" : ""}`}>
+      <ul className="nav-menu__list flex">
+        <span
+          className="hamburger-menu material-symbols-outlined"
+          onClick={() => setVisible(!visible)}
+        >
+          menu
+        </span>
+        <li className={"nav-menu__item" + visibileClassName}>
+          <Link href="/">
+            <a className="nav-menu__link">Home</a>
+          </Link>
+        </li>
+        <li className={"nav-menu__item" + visibileClassName}>
+          <Link href="/projects">
+            <a className="nav-menu__link">Projects</a>
+          </Link>
+        </li>
+        <li className={"nav-menu__item" + visibileClassName}>
+          <Link href="/blogs">
+            <a className="nav-menu__link">Blogs</a>
+          </Link>
+        </li>
+        <li className={"nav-menu__item" + visibileClassName}>
+          <Link href="/contact-me">
+            <a className="nav-menu__link">Contact Me</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
