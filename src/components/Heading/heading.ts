33646e9d74a40d038ede60312d6f10b038df8e54
David Taing <adavidtaing@gmail.com>
2022-11-22 22:02:51 +1100

    fix: NavItems showing by defualt on mobile

--- src/components/Heading/heading.ts
+++ src/components/Heading/heading.ts
@@ -2,11 +2,13 @@ const navLinks = document.querySelector(".nav-links");
 
 document.querySelector(".hamburger")?.addEventListener("click", () => {
   navLinks?.classList.toggle("hidden");
+  navLinks?.classList.toggle("flex");
 });
 
 document.querySelectorAll(".nav-link").forEach((link) =>
   link.addEventListener("click", () => {
     navLinks?.classList.add("hidden");
+    navLinks?.classList.toggle("flex");
   })
 );
 
